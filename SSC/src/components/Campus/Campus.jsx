c5958d9ad555133700f9bda0e5a293daf26a5086
dheerajatmakuri <dheerajatmakuri10010@gmail.com>
2025-04-13 13:27:27 -0500

    updated insta url

--- SSC/src/components/Campus/Campus.jsx
+++ SSC/src/components/Campus/Campus.jsx
@@ -21,7 +21,7 @@ const Campus = () => {
             <img src={utd5} alt="" />
             <img src={utd6} alt="" />
         </div>
-        <a href="https://www.instagram.com/ut_dallas/?hl=en" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.instagram.com/sscatutd?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer">
             <button className='btn dark-btn'>See More Here <img src={white_arrow} alt="" /></button>
         </a>
     </div>
